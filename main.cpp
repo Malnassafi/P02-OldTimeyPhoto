a94b610841ac789c5d7e51468c5b74798d553975
Malnassafi <malnassafi@mail.csuchico.edu>
2017-10-06 10:26:18 -0700

    Project02: almost done :)

--- main.cpp
+++ main.cpp
@@ -9,26 +9,34 @@ int main()
     Bitmap image;
     vector <vector <Pixel> > bmp;
     Pixel rgb;
+    int Average;
 
     image.open("project2image.bmp");
     bmp = image.toPixelMatrix();
     
     cout<<"project2image.bmp has been loaded. It is "<<bmp[0].size()<<" pixels wide and "<<bmp.size()<<" pixels high."<<endl;
+    // cout<<bmp.size()<<" "<<endl; 
+    // cout<<bmp[0].size()<<" "<<endl; 
 
-
-   // cout<<bmp.size()<<" "<<endl;  //high
-   // cout<<bmp[0].size()<<" "<<endl;  //wide
-
-/*    for(int r=0; r<bmp.size();r++)
+    // do a for loop to add bmp[r][c].red + bmp[r][c].blue + bmp[r][c].green = average/3
+    //then make the average = bmp[r][c].red and bmp[r][c].blue and bmp[r][c].green
+    for(int r=0; r<bmp.size();r++)
       {
-        for(int c=0; c<bmp[r].size();c++)
-          {
-            cout<<bmp[r][c]<<" "<<endl;
-          }
+         for(int c=0; c<bmp[0].size();c++)
+             {
+                 rgb = bmp[r][c];
+                 Average = (rgb.red + rgb.green + rgb.blue)/3;
+                 // cout<<Average<<" "<<endl;
+                 rgb.red = Average;
+                 rgb.green = Average;
+                 rgb.blue = Average;
+                 bmp[r][c] = rgb
+             }
       }
-*/ 
+     
+    image.fromPixelMatrix(bmp);
+    image.save("oldtimey.bmp");
 
-    
- return 0;
+    return 0;
 }
 
