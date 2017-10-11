a27264c47d10f5464979be8904d8b35bb9626f3a
Malnassafi <malnassafi@mail.csuchico.edu>
2017-10-11 10:51:34 -0700

    im done

--- main.cpp
+++ main.cpp
@@ -11,17 +11,16 @@ int main()
     Pixel rgb;
     int average;
     string fileName;
-     // cout<<"project2image.bmp has been loaded. It is "<<bmp[0].size()<<" pixels wide and "<<bmp.size()<<" pixels high."<<endl;
-
+    bool validImage;
     do
     {
          cout<<"What's the file's name? [it must be in BMP format!]"<<endl;
          cin>>fileName;            //saveing the users input in fileName 
          image.open(fileName);     //reading  the file that the user inputed
-         image.isImage();          //cheaking if it is a valid image
-         if(image.isImage() == 1)  //if it is a valid image go through the if statement
+         validImage = image.isImage();          //cheaking if it is a valid image
+         if(validImage == true)  //if it is a valid image go through the if statement
          { 
-             cout<<fileName<<" has been loaded"<<endl;
+             cout<<fileName<<" has been loaded. It is "<<endl;
              bmp = image.toPixelMatrix(); //converting it to a pixel matrix
          }
              
@@ -30,11 +29,11 @@ int main()
              cout<<"The file must be a 24 bit depth Windows BMP file. Please try again ^.^\n";
          }
     
-    }while(image.isImage() == 0); // and also if the image is invalid it will loop again
+    }while(validImage == false); //also if the image is invalid it will loop again
      
     for(int r=0; r<bmp.size();r++) //this for loop will go throgh every row
     {
-        for(int c=0; c<bmp[0].size();c++) //this for loop will go throgh every coloum
+        for(int c=0; c<bmp[0].size();c++) //this for loop will go throgh every coloum in a specifed row
         {
             rgb = bmp[r][c]; //makeing rgb = bmp at row r and coloum c
             average = (rgb.red + rgb.green + rgb.blue)/3; //setting average equal to the average of the red, green and blue so we added them and devided by 3 to optain it
